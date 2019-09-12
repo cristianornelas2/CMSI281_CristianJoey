6e226af3d9030e12389a37d1bce55eb887e51390
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2019-09-12 14:41:17 -0700

    Update SourceFile.java

--- Classwork/Classwork02/copiersupport/SourceFile.java
+++ Classwork/Classwork02/copiersupport/SourceFile.java
@@ -31,40 +31,6 @@ public class SourceFile {
 
 	public void closeFile() throws FileNotFoundException, IOException{
 		reader.close();
-		System.out.println(fileSource + "is closing");
+		System.out.println(fileSource + " : shutting down");
 	}
 }
-
-
-
-
-
-
-	/* private File source;
-	public File target;
-	private FileReader reader;
-	private FileWriter writer;
-
-	public SourceFile (String inPath, String outPath){
-        source = new File(inPath);
-        target = new File(outPath);
-		
-
-		if(source.canRead(){
-			reader = new FileReader(source);
-			writer = new FileWriter(target);
-			int c;
-			while((c = reader.read()) != -1) {
-  	       		writer.write(c);
-			}
-		}
-		
-		if( reader != null) {
-			reader.close();
-		} 
-		if( writer != null) {
-			writer.close();
-		}
-	}
-}
-*/
