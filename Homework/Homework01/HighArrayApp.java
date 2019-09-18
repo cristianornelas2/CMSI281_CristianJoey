0f15c594d8a52d7ef3b7e52e1e5046afb9295333
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2019-09-18 14:04:14 -0700

    Update HighArrayApp.java

--- Homework/Homework01/HighArrayApp.java
+++ Homework/Homework01/HighArrayApp.java
@@ -12,7 +12,8 @@ class HighArrayApp
           HighArray arr;                  // reference to array
           arr = new HighArray(maxSize);   // create the array
 
-          arr.insert(77);                 // insert 10 items
+          System.out.print("--------TESTING FOR INSERT() ---------");
+          arr.insert(77);                 
           arr.insert(99); 
           arr.insert(44); 
           arr.insert(55); 
@@ -36,8 +37,48 @@ class HighArrayApp
           arr.delete(00);                // delete 3 items
           arr.delete(55); 
           arr.delete(99);
-
-          arr.display();                 // display items again
+      
+          System.out.println("--------Testing for GETMAX()--------");
+            arr.insert(50);
+          System.out.println("Expects: 50   Got: " + arr.getMax());
+            arr.delete(50);
+            arr.insert(75);
+          System.out.println("Expects: 75   Got:  " + arr.getMax());
+            arr.delete(75);
+            arr.insert(25);
+          System.out.println("Expects: 25   Got:  " + arr.getMax());
+            arr.delete(25);
+            arr.insert(100);
+          System.out.println("Expects: 100  Got:  " + arr.getMax());
+            arr.delete(100);
+      
+      
+         System.out.println("--------Testing for NODUPS()--------");
+            HighArray arr2 = new HighArray(20);
+            arr2.insert(5);
+            arr2.insert(10);
+            arr2.insert(15);
+            arr2.insert(20);
+            arr2.insert(20);
+            arr2.insert(25);
+            arr2.insert(25);
+            arr2.insert(25);
+            arr2.insert(30);
+            arr2.insert(35);
+            arr2.insert(40);
+            arr2.insert(45);
+            arr2.insert(45);
+            arr2.insert(50);
+            arr2.insert(55);
+            arr2.insert(60);
+            arr2.insert(65);
+            arr2.insert(70);
+            arr2.insert(70);
+            arr2.insert(75);
+            
+            arr2.display();
+            arr2.noDups();
+            arr2.display();
           } // end main()
          } 
          // end class HighArrayApp
