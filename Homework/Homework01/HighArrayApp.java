cdfea14248a2869d15381128c688297801f3df1a
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2019-09-18 12:56:11 -0700

    Create HighArrayApp.java

---
+++ Homework/Homework01/HighArrayApp.java
@@ -0,0 +1,45 @@
+// HighArrayApp.java
+// demonstrates array class with high-level interface
+// to run this program: C>java HighArrayApp
+////////////////////////////////////////////////////////////////
+import HighArray.java*
+
+class HighArrayApp
+   {
+   public static void main(String[] args)
+          {
+          int maxSize = 100;              // array size 
+          HighArray arr;                  // reference to array
+          arr = new HighArray(maxSize);   // create the array
+
+          arr.insert(77);                 // insert 10 items
+          arr.insert(99); 
+          arr.insert(44); 
+          arr.insert(55); 
+          arr.insert(22); 
+          arr.insert(88); 
+          arr.insert(11); 
+          arr.insert(00); 
+          arr.insert(66); 
+          arr.insert(33);
+
+          arr.display();
+
+          int searchKey = 35;
+          if( arr.find(searchKey)) {
+             System.out.println("Found " + searchKey); 
+          }
+          else {
+             System.out.println("Can’t find " + searchKey);
+          }
+
+          arr.delete(00);                // delete 3 items
+          arr.delete(55); 
+          arr.delete(99);
+
+          arr.display();                 // display items again
+          } // end main()
+         } 
+         // end class HighArrayApp
+
+////////////////////////////////////////////////////////////////
