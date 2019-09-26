c44b8da271eaf42258483d525cc68b7f4fe372e2
ppereir2 <ppereir2@lion.lmu.edu>
2019-09-26 15:49:44 -0700

    Adds IntQueue methods

--- Classwork/Classwork05/IntQueue.java
+++ Classwork/Classwork05/IntQueue.java
@@ -13,11 +13,64 @@
          return size;
       }
 
+      /*
+
       public void prepend( int dataToAdd ) {
          Node currentHead = head;
          head = new Node( dataToAdd );
          head.next = currentHead;
          size++;
+      }
+      */
+
+      public int peek(){
+          return head.data;
+      }
+
+      public void insert(int x){
+        Node newNode = new Node(x);
+        Node current = head;
+        while(current.next!=null){
+            current = current.next;
+        }
+        current.next = newNode;
+        size++;
+      }
+
+
+      /* this is for a stack
+
+      public int remove(){
+          Node current = head;
+          Node last = current;
+          while(current.next!=null){
+              last = current;
+              current = current.next;
+          }
+          int out = current.data;
+          last.next = null;
+          return out;
+      }
+
+      */
+
+      public boolean isEmpty(){
+          return (head==null) ? true : false;
+      }
+
+      public int remove(){
+        int out = -1;
+        if(head!=null)
+        {
+            out = head.data;
+            if (head.next!=null){
+                head = head.next;
+            }else{
+                head = null;
+            }
+            size--;
+        }
+        return out;
       }
 
       private class Node {
@@ -44,6 +97,9 @@
          return it;
       }
 
+      /*
+
+
       public void insertAt(int index, int data) {
            Node newNode = new Node(data);
            IntLinkedList.Iterator it = getIteratorAt(index+1);
@@ -56,6 +112,8 @@
          it.currentNode.next = it.currentNode.next.next;
 
       }
+
+      */
 
       public class Iterator {
          private Node currentNode;
