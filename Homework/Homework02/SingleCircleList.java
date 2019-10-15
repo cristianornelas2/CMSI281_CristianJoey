d562188e749cdff2ac02553419ade4831587fce9
ppereir2 <ppereir2@lion.lmu.edu>
2019-10-15 13:19:31 -0700

    Adds singly-linked circular list, completed but untested

---
+++ Homework/Homework02/SingleCircleList.java
@@ -0,0 +1,108 @@
+class SingleCircleList{
+    
+    class Node{
+
+        public Object data;
+        public Node next;
+
+        public Node(Object inD){
+            data = inD;
+            next = this;
+        }
+        public Node(Object inD, Node nextNode){
+            data = inD;
+            next = nextNode;
+        }
+    }
+
+    Node current;
+    int nItems;
+
+    public SingleCircleList(){
+        nItems = 0;
+    }
+    public SingleCircleList(Object startData){
+        current = new Node(startData);
+        nItems=1;
+    }
+
+    public void insert(Object data){
+        if(nItems>1){
+            Node fresh = new Node(data, current.next);
+            current.next = fresh;
+            cuurent = fresh;
+        }else if(nItems==1){
+            Node fresh = new Node(data, current);
+            current.next=fresh;
+            current = fresh;
+        }else{
+            current = new Node(data);
+        }
+        
+        nItems++;
+    }
+
+    public void step(){
+        if(nItems>1){
+            current = current.next;
+        }
+    }
+
+    public void remove(){
+        if(nItems<=1){
+            current = null;
+            nItems=0;
+        }else{
+            for(int i=0; i<nItems-1; i++){
+                step(); //move the pointer to one previous to current
+            }
+            current.next = current.next.next;
+            nItems--;
+        }
+    }
+
+    public Object peekCurrent(){
+        if(nItems>0){
+            return current.data;
+        }
+        else{
+            System.out.println("List too small to peek, returning null..." + "\n");
+            return new Object();
+        }
+    }
+
+    public boolean contains(Object targetData){
+        
+        int checked = 0;
+        while(checked<nItems){
+            if(current.data==targetData){
+                return true;
+            }else{
+                checked++;
+                step();
+            }
+        }
+
+        return false;
+    }
+
+
+    public void display(){
+        
+        String out = "This circular list contains " + nItems + "\n";
+        
+        out+="\n" + "The contents of this list are: " + "\n";
+
+        int toAdd = nItems;
+
+        while(toAdd>0){
+            out += "[" + current.data + "]" + "\n";
+            step();
+            toAdd--;
+        }
+        System.out.println(out);
+    }
+
+
+
+}
